0ca71b38b180db82fd82f4a71fec6756f0c8c06c
Jean-LucMartel <jmartel@csumb.edu>
2025-10-15 22:24:21 -0700

    changed build.gradle dependencies and added Security.Config.java

--- src/main/java/com/example/demo/auth/SecurityConfig.java
+++ src/main/java/com/example/demo/auth/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.example.demo.auth;
 
+import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(a -> a
+                        .requestMatchers("/", "/public/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());  // new
+        return http.build();
+    }
 }
